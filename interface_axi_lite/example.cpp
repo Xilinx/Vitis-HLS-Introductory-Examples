7c2535c7c0db22c6fe048286192555b9437ecd91
f-rivo <frederic@xilinx.com>
2020-08-13 17:55:24 -0700

    removed register option

--- interface_axi_lite/example.cpp
+++ interface_axi_lite/example.cpp
@@ -21,11 +21,8 @@ void example(char *a, char *b, char *c)
 {
 #pragma HLS INTERFACE s_axilite port=a bundle=BUS_A
 #pragma HLS INTERFACE s_axilite port=b bundle=BUS_A
-#pragma HLS INTERFACE s_axilite port=c register bundle=BUS_A
+#pragma HLS INTERFACE s_axilite port=c bundle=BUS_A
 #pragma HLS INTERFACE s_axilite port=return bundle=BUS_A
 
   *c += *a + *b;
 }
-  
-  
-
