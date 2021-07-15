69d2f566a14d0335ba627e89fcdc43473ddfdb00
Ramanan Radhakrishnan <ramananr@xilinx.com>
2021-07-15 11:52:14 -0700

    Added new tests and renamed one old test

---
+++ Dataflow/Channels/using_stream_of_blocks/diamond.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2021 Xilinx, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+#include "hls_stream.h"
+#include "hls_streamofblocks.h"
+
+#define N 100
+#define NUM_BLOCKS 10
+typedef unsigned char data_t;
+typedef unsigned char block_data_t[NUM_BLOCKS];
+
+// Top function
+void diamond(hls::stream<data_t> &vecIn, hls::stream<data_t> &vecOut);
+
+// Sub functions
+void funcA(hls::stream<data_t> &in, hls::stream_of_blocks<block_data_t> &out1,
+           hls::stream_of_blocks<block_data_t> &out2);
+void funcB(hls::stream_of_blocks<block_data_t> &in, hls::stream_of_blocks<block_data_t> &out);
+void funcC(hls::stream_of_blocks<block_data_t> &in, hls::stream_of_blocks<block_data_t> &out);
+void funcD(hls::stream_of_blocks<block_data_t> &in1, hls::stream_of_blocks<block_data_t> &in2,
+           hls::stream<data_t> &out);
