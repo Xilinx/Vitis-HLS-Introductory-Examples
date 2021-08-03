20dd7159f19a35fc2f52224e22734eb4a9cc5218
sai akhil <aayyagar@xilinx.com>
2021-08-03 16:06:58 -0700

    moved hierarchy func

---
+++ Pipelining/Functions/coding_hierarchy_func4/hier_func4.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2021 Xilinx, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef _hier_func4_H_
+#define _hier_func4_H_
+
+#include <stdio.h>
+
+#define NUM_TRANS 40
+
+typedef int din_t;
+typedef int dint_t;
+typedef int dout_t;
+
+void hier_func4(din_t A, din_t B, dout_t *C, dout_t *D);
+
+#endif
+
