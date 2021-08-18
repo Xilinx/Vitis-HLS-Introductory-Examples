03892a43e0856eaf4ed3e4623688468125cf4267
sai akhil <aayyagar@xilinx.com>
2021-08-18 15:09:55 -0700

    changes to remerge example

--- Modeling/free_running_kernel_variable_latency/proj_example/solution1/csim/build/sim.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# ==============================================================
-# Vitis HLS - High-Level Synthesis from C, C++ and OpenCL v2021.1 (64-bit)
-# Copyright 1986-2021 Xilinx, Inc. All Rights Reserved.
-# ==============================================================
-# The next line restarts using autoesl tclsh \
-    exec /proj/xbuilds/SWIP/2021.1_0610_2318/installs/lin64/Vitis_HLS/2021.1/bin/vitis_hls run_sim.tcl
