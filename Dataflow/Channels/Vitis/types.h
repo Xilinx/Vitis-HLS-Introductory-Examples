461b33d673af51f0182bc2748f5476584f918573
ravicho <44479645+ravicho@users.noreply.github.com>
2022-04-01 10:12:39 -0700

    Added Vitis Acceleration application

---
+++ Dataflow/Channels/Vitis/types.h
@@ -0,0 +1,6 @@
+#include <vector>
+
+#define totalNumWords 512
+// Each vector will be 64 bytes (16 x 4 bytes)
+// typedef std::vector<uint32_t, aligned_allocator<uint32_t>> vecOf16Words;
+//typedef std::vector<uint32_t, aligned_allocator<uint32_t>> vecTotalWords;
