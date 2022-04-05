91582654495426eebb64ea88c594c58142197b05
ravicho <44479645+ravicho@users.noreply.github.com>
2022-04-05 16:38:28 -0700

    Added read/write APIs

--- Dataflow/Channels/using_fifos/diamond.cpp
+++ Dataflow/Channels/using_fifos/diamond.cpp
@@ -35,74 +35,68 @@ void diamond(vecOf16Words* vecIn, vecOf16Words* vecOut, int size)
 
 void load(vecOf16Words *in, hls::stream<vecOf16Words >& out, int size)
 {
-Loop0:
+Loop_Ld:
   for (int i = 0; i < size; i++)
   {
-#pragma HLS pipeline II=1 rewind
-//#pragma HLS pipeline rewind
-//#pragma HLS unroll factor = 2
-    out << in[i];
+    #pragma HLS performance target_ti=32
+    #pragma HLS LOOP_TRIPCOUNT max=32
+    out.write(in[i]);
   }
 }
 
 void compute_A(hls::stream<vecOf16Words >& in, hls::stream<vecOf16Words >& out1, hls::stream<vecOf16Words >& out2, int size)
 {
-Loop0:
+Loop_A:
   for (int i = 0; i < size; i++)
   {
-#pragma HLS pipeline II=1 rewind
-//#pragma HLS pipeline rewind
-//#pragma HLS unroll factor = 2
+    #pragma HLS performance target_ti=32
+    #pragma HLS LOOP_TRIPCOUNT max=32
     vecOf16Words t = in.read();
-    out1 << t * 3;
-    out2 << t * 3;
+    out1.write(t * 3);
+    out2.write(t * 3);
   }
 }
 
 void compute_B(hls::stream<vecOf16Words >& in, hls::stream<vecOf16Words >& out, int size)
 {
-Loop0:
+Loop_B:
   for (int i = 0; i < size; i++)
   {
-#pragma HLS pipeline II=1 rewind
-//#pragma HLS pipeline rewind
-//#pragma HLS unroll factor = 2
-    out << in.read() + 25;
+    #pragma HLS performance target_ti=32
+    #pragma HLS LOOP_TRIPCOUNT max=32
+    out.write(in.read() + 25);
   }
 }
 
 
 void compute_C(hls::stream<vecOf16Words >& in, hls::stream<vecOf16Words >& out, int size)
 {
-Loop0:
+Loop_C:
   for (data_t i = 0; i < size; i++)
   {
-#pragma HLS pipeline II=1 rewind
-//#pragma HLS pipeline rewind
-//#pragma HLS unroll factor = 2
-    out << in.read() * 2;
+    #pragma HLS performance target_ti=32
+    #pragma HLS LOOP_TRIPCOUNT max=32
+    out.write(in.read() * 2);
   }
 }
 void compute_D(hls::stream<vecOf16Words >& in1, hls::stream<vecOf16Words >& in2, hls::stream<vecOf16Words >& out, int size)
 {
-Loop0:
+Loop_D:
   for (data_t i = 0; i < size; i++)
   {
-#pragma HLS pipeline II=1 rewind
-//#pragma HLS pipeline rewind
-//#pragma HLS unroll factor = 2
-    out<< in1.read() + in2.read();
+    #pragma HLS performance target_ti=32
+    #pragma HLS LOOP_TRIPCOUNT max=32
+    out.write(in1.read() + in2.read());
   }
 }
 
 void store(hls::stream<vecOf16Words >& in, vecOf16Words *out, int size)
 {
-Loop0:
+Loop_St:
   for (int i = 0; i < size; i++)
   {
-#pragma HLS pipeline II=1 rewind
-//#pragma HLS pipeline rewind
-//#pragma HLS unroll factor = 2
+    #pragma HLS performance target_ti=32
+    #pragma HLS LOOP_TRIPCOUNT max=32
     out[i] = in.read();
   }
 }
