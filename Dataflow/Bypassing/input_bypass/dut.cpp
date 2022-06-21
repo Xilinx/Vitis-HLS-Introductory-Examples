2f3f7862de7db4b78d88b144b163632dd158dbab
Ramanan Radhakrishnan <ramananr@xilinx.com>
2022-06-21 09:58:56 -0700

    Removed unnecessary static variable j and other commented out lines

--- Dataflow/Bypassing/input_bypass/dut.cpp
+++ Dataflow/Bypassing/input_bypass/dut.cpp
@@ -1,52 +1,46 @@
-
+ 
 
 void add_kernel(int tmp1[128], int tmp2[128], int tmp3[128])
 {
-//	static int j;
 
-	int buff[127];
-	for(int i=0;i<128;i++)
-	{
-		tmp3[i] = tmp1[i] +tmp2[i];
-	}
+    int buff[127];
+    for(int i=0;i<128;i++)
+    {
+        tmp3[i] = tmp1[i] +tmp2[i];
+    }
 
 }
 
 
 void double_pass(int b[128], int tmp2[128], int tmp1[128], int tmp4[128])
 {
-//	static int j;
 
-	int buff[127];
-	for(int i=0;i<128;i++)
-	{
-		tmp2[i] = b[i];
-		tmp4[i] = tmp1[i];
-	}
+    int buff[127];
+    for(int i=0;i<128;i++)
+    {
+        tmp2[i] = b[i];
+        tmp4[i] = tmp1[i];
+    }
 
 }
 
 void pass(int a[128], int tmp1[128])
 {
-	static int j;
-	for(int i=0;i<128;i++)
-	{
-		j++;
-		tmp1[i]= a[i];
-	}
+    for(int i=0;i<128;i++)
+    {
+        tmp1[i]= a[i];
+    }
 
 }
 
 void dut(int a[128], int b[128], int tmp3[128])
 {
-//#pragma HLS INTERFACE ap_ovld depth=512 port=a
-
 #pragma HLS DATAFLOW
 
-int tmp1[128],tmp2[128], tmp4[128];
+    int tmp1[128],tmp2[128], tmp4[128];
 
-	pass(a,tmp1);
-	double_pass(b,tmp2, tmp1, tmp4);
-	add_kernel(tmp4,tmp2,tmp3);
+    pass(a,tmp1);
+    double_pass(b,tmp2, tmp1, tmp4);
+    add_kernel(tmp4,tmp2,tmp3);
 
 }
--- Dataflow/Bypassing/input_bypass/dut_sol.cpp
+++ Dataflow/Bypassing/input_bypass/dut_sol.cpp
@@ -2,41 +2,35 @@
 
 void add_kernel(int tmp1[128], int tmp2[128], int tmp3[128])
 {
-	static int j;
 
-	int buff[127];
-	for(int i=0;i<128;i++)
-	{
-		j++;
-		tmp3[i] = tmp1[i] +tmp2[i];
-	}
+    int buff[127];
+    for(int i=0;i<128;i++)
+    {
+        tmp3[i] = tmp1[i] +tmp2[i];
+    }
 
 }
 
 
 void Double_pass(int tmp2[128], int tmp1[128], int tmp4[128], int tmp5[128])
 {
-	static int j;
 
-	int buff[127];
-	for(int i=0;i<128;i++)
-	{
-		j++;
-		tmp4[i] = tmp1[i];
-		tmp5[i] = tmp2[i];
-	}
+    int buff[127];
+    for(int i=0;i<128;i++)
+    {
+        tmp4[i] = tmp1[i];
+        tmp5[i] = tmp2[i];
+    }
 
 }
 
 void pass(int a[128], int b[128],int tmp1[128], int tmp2[128])
 {
-	static int j;
-	for(int i=0;i<128;i++)
-	{
-		j++;
-		tmp1[i]= a[i];
-		tmp2[i] = b[i];
-	}
+    for(int i=0;i<128;i++)
+    {
+        tmp1[i]= a[i];
+        tmp2[i] = b[i];
+    }
 
 }
 
@@ -44,9 +38,10 @@ void dut(int a[128], int b[128], int tmp3[128])
 {
 #pragma HLS DATAFLOW
 
-int tmp1[128],tmp2[128], tmp4[128];
-int tmp5[128];
-	pass(a,b,tmp1,tmp2);
-	Double_pass(tmp2,tmp1, tmp4, tmp5);
-	add_kernel(tmp4,tmp5,tmp3);
+    int tmp1[128],tmp2[128], tmp4[128];
+    int tmp5[128];
+
+    pass(a,b,tmp1,tmp2);
+    Double_pass(tmp2,tmp1, tmp4, tmp5);
+    add_kernel(tmp4,tmp5,tmp3);
 }
