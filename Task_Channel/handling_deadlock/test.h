04ecb5cdaf2639b789a1e166e09e4dcfe9ff0da2
Ramanan Radhakrishnan <ramananr@xilinx.com>
2022-09-15 15:39:06 -0700

    Added new test

--- Task_Channel/handling_deadlock/test.h
+++ Task_Channel/handling_deadlock/test.h
@@ -13,29 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include "test.h"
+#include "hls_task.h"
 
-int main() {
+#define N 100
 
-    hls::stream<int, N> in;
-    hls::stream<int, N/2> out1;
-    hls::stream<int, N/2> out2;
-
-  for (int i = 0; i < N; i++)
-    in.write(i);
-  
-  odds_and_evens(in, out1, out2);
-
-
-  int sum1 = 0, sum2 = 0;
-  for (int i = 0; i < N/2; i++)
-    sum1 += out1.read();
-  for (int i = 0; i < N/2; i++)
-    sum2 += out2.read();
-  
-  if ((sum1 != 30) && (sum2 != 30))
-      return 1;
-  
-  return 0;
-}
+void test(int *in, int *out, int n);
 
--- Task_Channel/handling_deadlock/test_tb.cpp
+++ Task_Channel/handling_deadlock/test_tb.cpp
@@ -16,26 +16,17 @@
 #include "test.h"
 
 int main() {
+  int in[N];
+  int out[N];
 
-    hls::stream<int, N> in;
-    hls::stream<int, N/2> out1;
-    hls::stream<int, N/2> out2;
-
-  for (int i = 0; i < N; i++)
-    in.write(i);
+  for (int i = 0; i < N; i++) {
+      in[i] = i;
+      out[i] = 0;
+  }
   
-  odds_and_evens(in, out1, out2);
+  test(in, out, 5);
 
-
-  int sum1 = 0, sum2 = 0;
-  for (int i = 0; i < N/2; i++)
-    sum1 += out1.read();
-  for (int i = 0; i < N/2; i++)
-    sum2 += out2.read();
-  
-  if ((sum1 != 30) && (sum2 != 30))
-      return 1;
-  
   return 0;
 }
+
 
