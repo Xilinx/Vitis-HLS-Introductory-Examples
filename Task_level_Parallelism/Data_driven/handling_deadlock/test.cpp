2897e2c510b54bc11026acf0bf179f60107e6e2b
Ramanan Radhakrishnan <ramananr@xilinx.com>
2022-10-14 10:38:58 -0700

    Switch static to hls_thread_local

--- Task_level_Parallelism/Data_driven/handling_deadlock/test.cpp
+++ Task_level_Parallelism/Data_driven/handling_deadlock/test.cpp
@@ -49,14 +49,14 @@ void test(int *in, int *out, int n) {
 #pragma HLS interface m_axi port=in depth=100
 #pragma HLS interface m_axi port=out depth=100
 #pragma HLS dataflow
-    static hls::stream<int, 100> s1;
-    static hls::stream<int, 100> s2_0, s2_1, s2_2;
-    static hls::stream<int, 100> s3;
+    hls_thread_local hls::stream<int, 100> s1;
+    hls_thread_local hls::stream<int, 100> s2_0, s2_1, s2_2;
+    hls_thread_local hls::stream<int, 100> s3;
 
-    static hls::task t2(func2, s2_2, s3); 
-    static hls::task t1(func1, s1, s2_0); 
-    static hls::task t3_1(func3, s2_0, s2_1); 
-    static hls::task t3_2(func3, s2_1, s2_2); 
+    hls_thread_local hls::task t2(func2, s2_2, s3); 
+    hls_thread_local hls::task t1(func1, s1, s2_0); 
+    hls_thread_local hls::task t3_1(func3, s2_0, s2_1); 
+    hls_thread_local hls::task t3_2(func3, s2_1, s2_2); 
 
     read_in (in, n, s1);
     write_out (out, n, s3);
