c3eb90d10c0f63a36c4ebb07bd88e5b6d09d7400
Herve R @ Xilinx <herver@xilinx.com>
2022-11-09 13:27:53 +0000

    minor edits, no function changes

--- Interface/Memory/coefficient_filter/hamming_window.c
+++ Interface/Memory/coefficient_filter/hamming_window.c
@@ -28,7 +28,7 @@ void hamming_window(int32_t outdata[WINDOW_LEN], int16_t indata[WINDOW_LEN])
    // initialized as a ROM, it is recommended that the array initialization
    // be done in a sub-function with global (wrt this source file) scope.
    hamming_rom_init(window_coeff);
-process:
+process_frame:
    for (i = 0; i < WINDOW_LEN; i++) {
 #pragma HLS pipeline
       outdata[i] = (int32_t)window_coeff[i] * (int32_t)indata[i];
