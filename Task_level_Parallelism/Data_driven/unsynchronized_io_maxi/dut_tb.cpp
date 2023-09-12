23669b81527ba6233df318a4a34f5bd0f66aed80
Sai Akhil Ayyagari <aayyagar@xilinx.com>
2023-09-12 10:46:24 -0700

    lucianos updates

--- Task_level_Parallelism/Data_driven/unsynchronized_io_maxi/dut_tb.cpp
+++ Task_level_Parallelism/Data_driven/unsynchronized_io_maxi/dut_tb.cpp
@@ -2,36 +2,31 @@
 #include "ap_int.h"
 #include "hls_stream.h"
 #include <unistd.h>
-#define DWIDTH 32
-typedef ap_axiu<DWIDTH, 0, 0, 0> trans_pkt;
 
-extern "C" void increment(hls::stream<trans_pkt> &input,
-                          hls::stream<trans_pkt> &output, int *out, int *in);
 
-int main(int argc, char *argv[]) {
-  int size = 1024;
+extern "C" void stable_pointer(int* mem, hls::stream<int>&, hls::stream<int>&);
 
-  int memory_out[size];
-  int memory_in[size];
-  // memset(memory, 0, 128*256/8);
-  hls::stream<trans_pkt> in, out;
-  trans_pkt tmp;
-  trans_pkt val;
-  for (int i = 0; i < size; i++) {
-    tmp.data = i;
-    in.write(tmp);
-    memory_out[i] = 0;
-    memory_in[i] = 0;
-  }
 
-  increment(in, out, memory_out, memory_in);
+int main(int argc, char *argv[])
+{
+	int memory[512];
+	//memset(memory, 0, 128*256/8);
+	hls::stream<int> in, out;
+	int val;
+	for(int i = 0; i < 100; i++) {
+		in.write(i);
+        memory[i] = 0;
+	}
 
-  for (int i = 0; i < 100; i++) {
-    out.read(val);
-    if (val.data != i * 2) {
-      printf("%d != %d\n", val.data, i);
-      return 1;
-    }
-  }
-  return 0;
+    stable_pointer(memory, in, out);
+
+	for(int i = 0; i < 100; i++) {
+		out.read(val);
+        if (val != i * 23 * 11) {
+            printf("%d != %d\n", val, i * 23 * 11);
+            return 1;
+        }
+	}
+
+	return 0;
 }
