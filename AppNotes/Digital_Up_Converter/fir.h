99007fc12a35f46fc1af506af630d23d43e27594
Frederic Rivoallon <frederic.rivoallon@amd.com>
2024-06-03 13:32:18 -0700

    Inserted C++ Copyright headers

--- AppNotes/Digital_Up_Converter/fir.h
+++ AppNotes/Digital_Up_Converter/fir.h
@@ -1,12 +1,19 @@
-//______________________________________________________________________________
-// fir.h:
-// - various FIR module
-//
-//
-// a. paek, sep 2013
-//______________________________________________________________________________
-
-// some classes removed from xapp1299 reference code.
+/*
+ * Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+ * Copyright 2022-2024 Advanced Micro Devices, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #ifndef _FIR_H
 #define _FIR_H
