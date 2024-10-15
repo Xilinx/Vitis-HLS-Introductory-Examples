5419e0e9b6740cc1ae2cccfbc2475e12c5a09728
Nishit Mehta <nishit.mehta@amd.com>
2024-10-14 17:38:27 -0700

    initial commit

---
+++ Migration/python_scripts/run_with_external_config.py
@@ -0,0 +1,21 @@
+import vitis
+import os
+
+cwd = os.getcwd()+'/'
+
+# Initialize session
+client = vitis.create_client()
+client.set_workspace(path='./w')
+
+# Delete the component if it already exists
+if os.path.exists('./w/pointer_basic'):
+	client.delete_component(name='pointer_basic')
+
+# Create component. Create new config file in the component folder of the workspace
+comp = client.create_hls_component(name='pointer_basic', cfg_file = [cwd+'hls_config.cfg'], template = 'empty_hls_component')
+
+# Run flow steps
+comp = client.get_component(name='pointer_basic')
+comp.run(operation='C_SIMULATION')
+comp.run(operation='SYNTHESIS')
+comp.run(operation='CO_SIMULATION')
