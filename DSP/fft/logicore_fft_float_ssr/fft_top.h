9108f6d8d414529dded789d9a59f6314806641f4
f-rivo <frederic@xilinx.com>
2025-12-09 19:59:43 -0800

    Add logicore_fft_float_ssr example with configurable array/stream interface
    - New FFT example with SSR=2 floating-point implementation
    - Supports both array and stream interfaces via USE_STREAM_INTERFACE macro
    - Includes 5 comprehensive test cases: ramp, sine, sawtooth, mixed, impulse
    - Python scripts for test data generation and visualization
    - Complete testbench with automatic interface detection
    - Documentation and run scripts for both Python and Tcl flows

---
+++ DSP/fft/logicore_fft_float_ssr/fft_top.h
@@ -0,0 +1,63 @@
+/*
+ * Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+ * Copyright 2022-2025 Advanced Micro Devices, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "hls_fft.h"
+
+// User configurable: Choose interface type
+// Set to 1 for STREAM interface, 0 for ARRAY interface
+#ifndef USE_STREAM_INTERFACE
+#define USE_STREAM_INTERFACE 1
+#endif
+
+// Example configurable params
+#define FFT_SSR 2 // Super Sample Rate
+#define FFT_NFFT_MAX 10 // 1024-point FFT
+
+const char FFT_INPUT_WIDTH = 32;
+const char FFT_OUTPUT_WIDTH = FFT_INPUT_WIDTH;
+const int FFT_LENGTH = 1 << FFT_NFFT_MAX;
+
+#include <complex>
+using namespace std;
+
+// FFT configuration struct, inheriting from hls::ip_fft::ssr_params_t
+// Here we set SSR and specify floating point
+struct config1 : hls::ip_fft::ssr_params_t {
+    static const unsigned super_sample_rate = FFT_SSR;
+    static const bool use_native_float = true;
+    OVERRIDE(systolicfft_inv) = false;  // OVERRIDE as an option to avoid setting up misspelled parameters
+    static const unsigned log2_transform_length = FFT_NFFT_MAX;
+};
+
+typedef hls::ip_fft::config_t<config1> config_t;
+typedef hls::ip_fft::status_t<config1> status_t;
+
+typedef float data_in_t;
+typedef float data_out_t;
+
+typedef std::complex<data_in_t> cmpxDataIn;
+typedef std::complex<data_out_t> cmpxDataOut;
+
+// Function declaration with interface determined by macro
+#if USE_STREAM_INTERFACE
+void fft_top(bool direction,
+             hls::stream<cmpxDataIn> &in,
+             hls::stream<cmpxDataOut> &out,
+             bool* status);
+#else
+void fft_top(bool direction, cmpxDataIn in[FFT_LENGTH],
+             cmpxDataOut out[FFT_LENGTH], bool* status);
+#endif
