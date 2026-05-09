a7ce74c35bfb348c19f4dd20ca7ec4019d2f21a9
Nishit Mehta <nishit.mehta@amd.com>
2026-05-08 23:05:09 -0700

    renamed examples

---
+++ Interface/Streaming/axis_array_stream_no_side_channel_data/example.cpp
@@ -0,0 +1,27 @@
+/*
+ * Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+ * Copyright 2022-2025 Advanced Micro Devices, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+void example(int A[50], int B[50]) {
+#pragma HLS INTERFACE axis port = A
+#pragma HLS INTERFACE axis port = B
+
+    int i;
+
+    for (i = 0; i < 50; i++) {
+        B[i] = A[i] + 5;
+    }
+}
---
+++ Modeling/using_cpp_templates/cpp_template.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+ * Copyright 2022-2025 Advanced Micro Devices, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef _CPP_TEMPLATE_H_
+#define _CPP_TEMPLATE_H_
+
+#include <cstdlib>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+using namespace std;
+
+#define FIB_N 5
+
+typedef int data_t;
+
+void cpp_template(data_t a, data_t b, data_t& dout);
+
+#endif
